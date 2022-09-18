9fdba6519b4e73c819def38b61cac5b9b71c9e9c
letstrie <sakibkhan111296@gmail.com>
2022-09-18 11:10:43 +0600

    android basic

--- App.js
+++ App.js
@@ -1,20 +1,135 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useCallback } from 'react';
+import { Button, View, Text } from 'react-native';
+import AppLoading from 'expo-app-loading';
+
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './App/redux/store';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { ScreenContainer } from './App/components/screenContainer';
+import LoginSignupComponent from './App/screens/onboarding/loginSignup';
+import { DemographicInformation } from './App/screens/onboarding/demographicInformation';
+import { Homepage } from './App/screens/homepage';
+import { SpecificIssues } from './App/screens/specificIssues';
+
+import AssessmentList from './App/screens/assessments/AssessmentList.js';
+import Scale from './App/screens/assessments/Scale';
+import { ScaleDescriptionPage } from './App/screens/assessments/ScaleDescriptionPage';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import colors from './App/config/colors';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [fontsLoaded, error] = useFonts({
+    'poppins-bold': require('./App/assests/fonts/Poppins/Poppins-Bold.ttf'),
+    'poppins-regular': require('./App/assests/fonts/Poppins/Poppins-Regular.ttf'),
+    'playfair-black': require('./App/assests/fonts/PlayfairDisplay/PlayfairDisplay-Black.ttf'),
+    'playfair-bold': require('./App/assests/fonts/PlayfairDisplay/PlayfairDisplay-Bold.ttf'),
+    openSans: require('./App/assests/fonts/Open_Sans/OpenSans.ttf'),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScreenContainer onLayout={onLayoutRootView}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <NavigationContainer>
+            <Stack.Navigator
+              initialRouteName="LoginSignup"
+              screenOptions={{
+                headerStyle: {
+                  backgroundColor: colors.primary,
+                  height: 60,
+                },
+                headerTitleStyle: {
+                  color: 'white',
+                  fontSize: 20,
+                },
+                headerTintColor: 'white',
+                headerBackTitle: 'Back',
+                headerTitleAlign: 'center',
+              }}
+            >
+              <Stack.Screen
+                name="LoginSignup"
+                component={LoginSignupComponent}
+                options={{ headerShown: false }}
+              />
+              <Stack.Screen
+                name="DemographicInformation"
+                component={DemographicInformation}
+                options={{ headerShown: false }}
+              />
+
+              <Stack.Screen
+                name="Homepage"
+                component={Homepage}
+                options={({ navigation }) => ({
+                  title: 'Quality Life',
+                  headerShown: false,
+                })}
+              />
+
+              <Stack.Screen
+                name="AssessmentList"
+                component={AssessmentList}
+                options={({ navigation }) => ({
+                  title: 'Assessment Lists',
+                })}
+              />
+
+              <Stack.Screen
+                name="SpecificIssues"
+                component={SpecificIssues}
+                options={({ navigation }) => ({
+                  headerShown: false,
+                })}
+              />
+              {/* ExploreScale */}
+              <Stack.Screen
+                name="ExploreScale"
+                component={Scale}
+                options={({ navigation }) => ({
+                  title: 'Scale',
+                })}
+              />
+              {/* ScaleDescriptionPage */}
+              <Stack.Screen
+                name="ScaleDescriptionPage"
+                component={ScaleDescriptionPage}
+                options={({ navigation }) => ({
+                  title: 'Scale Description Page',
+                })}
+              />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </PersistGate>
+      </Provider>
+    </ScreenContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
